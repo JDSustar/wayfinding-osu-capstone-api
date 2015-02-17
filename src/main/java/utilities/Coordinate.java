0707207554758a242b10f064cc9a62ebe534637d
Tommy Forte <forte.39@osu.edu>
2015-02-16 22:33:40 -0500

    Created coordinate class 2

--- src/main/java/utilities/Coordinate.java
+++ src/main/java/utilities/Coordinate.java
@@ -4,4 +4,85 @@ package utilities;
  * Created by tjf3191 on 2/16/15.
  */
 public class Coordinate {
+
+    /**
+     * TYPE is for the constructor to differ which data is the original data.
+     */
+    public static enum TYPE {
+        NAD_27_TO_GCS,
+        GCS_TO_NAD_27
+    };
+
+    /**
+     * The private class data fields.
+     */
+    private double eastling;
+    private double northling;
+    private double latitude;
+    private double longitude;
+
+    /**
+     * The default constructor
+     */
+    public Coordinate() {
+        this.eastling = 0.0;
+        this.northling = 0.0;
+        this.latitude = 0.0;
+        this.longitude = 0.0;
+    }
+
+    /**
+     * The primary constructor to build a coordinate and calculate the opposing format
+     * @param x the x value of the coordinate, either eastling(feet) or longitude(degrees)
+     * @param y the y value of the coordinate, either northling(feet) or latitude(degrees)
+     * @param type the type of conversion to calculate
+     */
+    public Coordinate (double x, double y, Coordinate.TYPE type) {
+
+        double[] results;
+
+        if (type == TYPE.NAD_27_TO_GCS) {
+            this.eastling = x;
+            this.northling = y;
+            results = Utility.Nad27toGCS(eastling, northling);
+            this.latitude = results[1];
+            this.longitude = results[0];
+
+        } else if (type == TYPE.GCS_TO_NAD_27) {
+            this.latitude = y;
+            this.longitude = x;
+            results = Utility.GCStoNad27(this.latitude, this.longitude);
+            this.eastling = results[1];
+            this.northling = results[0];
+        }
+    }
+
+    /**
+     * @return the eastling value
+     */
+    public double getEastling(){
+        return this.eastling;
+    }
+
+    /**
+     * @return the northling value
+     */
+    public double getNorthling(){
+        return this.northling;
+    }
+
+    /**
+     * @return the latitude value
+     */
+    public double getLatitude(){
+        return this.latitude;
+    }
+
+    /**
+     * @return the longitude value
+     */
+    public double getLongitude(){
+        return this.longitude;
+    }
+
 }
--- src/main/java/utilities/Utility.java
+++ src/main/java/utilities/Utility.java
@@ -1,7 +1,44 @@
 package utilities;
 
+import java.awt.geom.Point2D;
+
 /**
  * Created by tjf3191 on 2/16/15.
  */
 public class Utility {
+
+    // Class variable for calculating the projection
+    //private static final Projection nad27= ProjectionFactory.getNamedPROJ4CoordinateSystem("nad27:3402");
+
+    // Class variable for converting survey feet to meters
+    public static final double survey2meter = 1200.0 / 3937.0;
+
+    /**
+     * Class method for converting a nad27 measure of eastling and northling
+     * values to GCS coordinates.
+     * @param eastling  the X value in feet
+     * @param northling the Y value in feet
+     * @return          returns a double point, at [0] is longitude and [1] is latitude
+     */
+    public static final double[] Nad27toGCS (double eastling, double northling) {
+
+        Point2D.Double result = new Point2D.Double(0.0, 0.0);
+
+        // Using library for conversion
+        //nad27.inverseTransform(new Point2D.Double(eastling * survey2meter, northling * survey2meter), result);
+
+        // Standard correction to coordinates
+        double latitude = result.getY() + 0.00049;
+        double longitude = result.getX() + 0.11241;
+
+        return new double[]{longitude, latitude};
+    }
+
+    public static final double[] GCStoNad27 (double lat, double lng) {
+
+        // Using library for conversion
+
+
+        return new double[]{};
+    }
 }

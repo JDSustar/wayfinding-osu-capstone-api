f0210febb69aebc3ea545f30d507225038c5a9b7
Jason D. Sustar <jason.sus@gmail.com>
2015-03-02 22:57:43 -0500

    Added toString() to Location.

--- src/main/java/com/Location.java
+++ src/main/java/com/Location.java
@@ -22,4 +22,10 @@ public class Location extends Node{
     {
         return name;
     }
+
+    @Override
+    public String toString()
+    {
+        return "Location: " + this.name + " -- Lat: " + this.getCoordinate().getLatitude() + " Long: " + this.getCoordinate().getLongitude();
+    }
 }
